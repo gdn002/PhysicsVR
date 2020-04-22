a191420bb2b140b8d0f510744c1fe2e6fc178a45
Guilherme Diegoli Neto <gd_neto@hotmail.com>
2020-04-22 20:29:43 +0200

    Prototype physics
    Very basic physics implementation, meant to be built upon for the project

---
+++ Assets/VirtualHandBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class VirtualHandBehaviour : MonoBehaviour
+{
+    public Transform parent;
+
+    public SteamVR_Action_Vibration Haptics;
+    public SteamVR_Input_Sources handType;
+
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(parent.position);
+        rigidbody.MoveRotation(parent.rotation);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Haptics.Execute(0, 0, 50, 1, handType);
+
+        Debug.Log("Impact relative velocity: " + collision.relativeVelocity.magnitude);
+    }
+}
